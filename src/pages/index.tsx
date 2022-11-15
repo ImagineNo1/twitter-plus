cc0a5442ca9fb950fbb3b5ca1617acd842331800
Mohammad Rezvani <mohammadrezvani2002@gmail.com>
2022-11-15 22:08:54 +0330

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Twitter Plus</title>
+      </Head>
+      <div>Welcome To Twitter Plus</div>
+    </>
+  );
+}
