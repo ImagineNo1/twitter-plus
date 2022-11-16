5e99bbaec4234d9c3ce57eea1def564f813ce012
Mohammad Rezvani <mohammadrezvani2002@gmail.com>
2022-11-16 19:06:47 +0330

    base design

--- src/components/NewTweet.tsx
+++ src/components/NewTweet.tsx
@@ -3,11 +3,7 @@ const NewTweet = () => {
     <div className="h-56 border-b-2 border-gray-600 px-2 relative">
       <div>
         <p className="pt-2 pl-2 text-xl font-bold">What's happening ?</p>
-        <textarea
-          name=""
-          id=""
-          className="block my-3 border-2 border-gray-400 w-full p-2 h-28 rounded-lg"
-        ></textarea>
+        <textarea className="block my-3 border-2 border-gray-400 w-full p-2 h-28 rounded-lg"></textarea>
       </div>
       <div className="absolute bottom-0 right-0 border-2 border-blue-400 rounded-lg px-5 cursor-pointer hover:bg-blue-300 hover:text-white py-1 text-lg mb-2">
         Tweet
